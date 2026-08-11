a3de5fb36080428493ff48aebb64bfc9b4425fbf
code-beast1122 <ahsanali.lhr99@gmail.com>
2026-08-11 20:24:52 +0500

    0.1.5

--- bitengine/__init__.py
+++ bitengine/__init__.py
@@ -4,8 +4,8 @@ BitEngine - A lightweight, high-performance key-value database engine.
 
 from .engine import BitEngine
 from .cli import run_cli as main_cli
-from .server import main as main_server
+from .server import run_server as main_server
 from .client import run_client as main_client
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ["BitEngine", "main_cli", "main_server", "main_client"]

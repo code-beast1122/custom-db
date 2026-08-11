d12cb6e0b00888bee0ae1743669e7d80442bb776
code-beast1122 <ahsanali.lhr99@gmail.com>
2026-08-11 20:38:21 +0500

    0.1.7

--- bitengine/__init__.py
+++ bitengine/__init__.py
@@ -7,5 +7,5 @@ from .cli import run_cli as main_cli
 from .server import run_server as main_server
 from .client import run_client as main_client
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __all__ = ["BitEngine", "main_cli", "main_server", "main_client"]
